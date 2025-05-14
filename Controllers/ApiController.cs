98e75380cce6709024dca17b677bcf172ad333cb
vissrobotic-admin <serdar.mentes71@gmail.com>
2025-05-14 15:28:21 +0300

    lokalden degısıklık

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -68,43 +68,7 @@ namespace RaspberryPiControl.Controllers
               }
           }
 
-       /* [HttpPost]
-
-        public async Task<IActionResult> UpdateRelayStatus([FromBody] StatusUpdateRequest request)
-        {
-            try
-            {
-                string jsonPath = Path.Combine(_webHostEnvironment.ContentRootPath, "devicedata.json");
-                string jsonContent = await System.IO.File.ReadAllTextAsync(jsonPath);
-
-                var options = new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true,
-                    WriteIndented = true
-                };
-
-                var deviceData = JsonSerializer.Deserialize<DeviceData>(jsonContent, options) ?? new DeviceData();
-
-                var device = deviceData.Devices.FirstOrDefault(d => d.IpAddress.Equals(request.IpAddress, StringComparison.OrdinalIgnoreCase));
-                if (device != null)
-                {
-                    device.AccessStatus = request.AccessStatus.ToLower() == "open" ? "Open" : "Closed";
-                    device.LastUpdate = DateTime.UtcNow;
-
-                    string updatedJson = JsonSerializer.Serialize(deviceData, options);
-                    await System.IO.File.WriteAllTextAsync(jsonPath, updatedJson);
-
-                    _logger.LogInformation($"Updated Relay status for device {request.IpAddress} to {request.Status}");
-                    return Ok(new { success = true });
-                }
-
-                return NotFound(new { success = false, message = "Device not found" });
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error updating Relay  device status");
-                return StatusCode(500, new { success = false, message = "Internal server error" });
-            }*/
+       
         }
 
     }
